e6187cd5bd3989482a835575c7be31b91cfaac10
ukmrs <murias.wstork@gmail.com>
2022-11-14 17:17:57 +0100

    packing RNA into the tree structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub mod forest;
 
+use forest::{DotBracket, Tree}; // admittedly "from forest import tree" would be funnier
+
 type PairList = Vec<Option<usize>>;
 
 #[allow(dead_code)]
@@ -25,4 +27,51 @@ pub fn get_pair_list(secondary_structure: &str) -> PairList {
 
     assert!(deck.is_empty());
     lovers
+}
+
+fn stem_walk(
+    mut tree: Tree<DotBracket>,
+    pair_list: &PairList,
+    pos: usize,
+    tail: usize,
+) -> (Tree<DotBracket>, usize) {
+    if pair_list[pos] == Some(tail) {
+        let node_ix = tree.sprout(DotBracket::newsome(pos, tail));
+        let (mut tree, ix) = stem_walk(tree, pair_list, pos + 1, tail - 1);
+        tree[node_ix].push(ix);
+        (tree, node_ix)
+    } else {
+        let node_ix = tree.sprout(DotBracket::new_loop());
+        rna_walk(tree, pair_list, node_ix, pos, tail)
+    }
+}
+
+fn rna_walk(
+    mut tree: Tree<DotBracket>,
+    pair_list: &PairList,
+    root_ix: usize,
+    pos: usize,
+    tail: usize,
+) -> (Tree<DotBracket>, usize) {
+    let mut pos = pos;
+    while pos <= tail {
+        if let Some(x) = pair_list[pos] {
+            let node_ix: usize;
+            (tree, node_ix) = stem_walk(tree, pair_list, pos, x);
+            tree[root_ix].push(node_ix);
+            pos = x + 1
+        } else {
+            let node_ix = tree.sprout(DotBracket::new(Some(pos), None));
+            tree[root_ix].push(node_ix);
+            pos += 1;
+        }
+    }
+    (tree, root_ix)
+}
+
+pub fn construct_tree(pair_list: &PairList) -> Tree<DotBracket> {
+    let mut tree = Tree::default();
+    let root_ix = tree.sprout(DotBracket::new_loop());
+    (tree, _) = rna_walk(tree, pair_list, root_ix, 0, pair_list.len() - 1);
+    tree
 }
--- tests/second_structure_check.rs
+++ tests/second_structure_check.rs
@@ -91,12 +91,12 @@ fn compare_trees<P: AsRef<Path>>(tree_file: P, tree: Tree<DotBracket>) {
 
         for kid in &treenode.offspring {
             let db = DotBracket::new(offspring.next().unwrap(), offspring.next().unwrap());
-            assert_eq!(tree[*kid].val, db)
+            assert_eq!(tree[*kid].val, db);
         }
 
         // check if the offspring in the test tree was properly exhausted
         // if there were no kids first next() won't be None but ""
-        assert!(offspring.next().is_none() || offspring.next().is_none())
+        assert!(offspring.next().is_none() || offspring.next().is_none());
     }
 }
 
@@ -104,4 +104,40 @@ fn compare_trees<P: AsRef<Path>>(tree_file: P, tree: Tree<DotBracket>) {
 fn tree_parsing_test() {
     let tree = make_simple_tree();
     compare_trees("tests/extras/fribo_tree", tree);
+}
+
+fn get_set_of_testfilses() -> Vec<Vec<PathBuf>> {
+    let mut test_set: Vec<Vec<PathBuf>> = vec![];
+    let trees_paths = Path::new("tests/sample_rnas/")
+        .read_dir()
+        .expect("oof")
+        .map(|x| x.unwrap().path().read_dir().unwrap());
+
+    for direcory in trees_paths {
+        let mut file_set: Vec<PathBuf> = vec![PathBuf::default(), PathBuf::default()];
+        for file in direcory.map(|x| x.unwrap().path()) {
+            if file.ends_with("tree") {
+                file_set[1] = file.to_owned();
+            } else if file.ends_with("seq_struct_pair") {
+                file_set[0] = file.to_owned();
+            }
+        }
+        test_set.push(file_set);
+    }
+    test_set
+}
+
+#[test]
+fn tree_creation_test() {
+    let testfiles = get_set_of_testfilses();
+    for rna_case in &testfiles {
+        let lines = read_lines(&rna_case[0]).unwrap();
+        let pair_list = lines.skip(2).next().unwrap().unwrap();
+        let pair_list: Vec<Option<usize>> = pair_list
+            .split(",")
+            .map(|x| x.parse::<usize>().ok())
+            .collect();
+        let tree = rnapkin::construct_tree(&pair_list);
+        compare_trees(&rna_case[1], tree);
+    }
 }
