9b6e1ff5e2747d3e663b0a030b374e31c83730a5
ukmrs <murias.wstork@gmail.com>
2022-11-22 01:31:27 +0100

    clean: rid lib.rs of dead code

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,3 @@ pub mod draw;
 pub mod forest;
 pub mod rnamanip;
 pub mod utils;
-use rnamanip::Nucleotide;
-
-#[allow(dead_code)]
-const SEQ: &str = "CGCUUCAUAUAAUCCUAAUGAUAUGGUUUGGGAGUUUCUACCAAGAGCCUUAAACUCUUGAUUAUGAAGUG";
-#[allow(dead_code)]
-const SST: &str = "...(((((((..((((((.........))))))......).((((((.......))))))..))))))...";
-#[allow(dead_code)]
-const FSEQ: &str =
-    "UUAUAGGCGAUGGAGUUCGCCAUAAACGCUGCUUAGCUAAUGACUCCUACCAGUAUCACUACUGGUAGGAGUCUAUUUUUUU";
-#[allow(dead_code)]
-const FSST: &str =
-    ".....(((((......)))))......(((....)))....((((((((((((((....)))))))))))))).........";
-#[allow(dead_code)]
-const TSEQ: &str = "GCAGAACAATTCAATATGTATTCGTTTAACCACTAGGGGTGTCCTTCATAAGGGCTGAGATAAAAGTGTGACTTTTAGACCCTCATAACTTGAACAGGTTCAGACCTGCGTAGGGAAGTGGAGCGGTATTTGTGTTATTTTACTATGCCAATTCCAAACCACTTTTCCTTGCGGGAAAGTGGTTTTTTTA";
-#[allow(dead_code)]
-const TOFFSST: &str = ".........(((..((((((...((((((((.....((((((((((...)))))).....(((((((...))))))).))))(((.....)))...)))).)))).))))))..)))..((((.(((((..(((......))).)))))..))))(((((((((((((....))))))))))))).....";
-#[allow(dead_code)]
-const TONSST: &str = "...(((((((.((...)).))).))))(((((((..((((((((((...)))))).....(((((((...))))))).))))(((.....))).(((((....)))))(((((((((((((((.(((((..(((......))).)))))..))))).......))))))))))....)))))))......";
-
-#[allow(unused_variables)]
-pub fn run() {
-    let pl = rnamanip::get_pair_list(TOFFSST);
-    let tree = forest::grow_tree(&pl);
-    let seq: Vec<Nucleotide> = TSEQ
-        .chars()
-        .map(|c| Nucleotide::from_char(c).expect("invalid nt!"))
-        .collect();
-
-    let bubbles = draw::gather_bubbles(&tree, &seq, 0.5);
-}
